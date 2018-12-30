6187bfe68d1e2f4ee2913cc8412cc9028ad63141
haohangxu <haohang.xu@gmail.com>
2018-12-30 15:19:21 -0500

    adding more exercises
    Signed-off-by: haohangxu <haohang.xu@gmail.com>

--- 02-exercises/06-pattern-matching/problem.ml
+++ 02-exercises/06-pattern-matching/problem.ml
@@ -11,6 +11,14 @@ let is_superman x =
   | _ -> false
 ;;
 
+(* We can also pattern match on multiple values at the same time. *)
+let is_same_person x y = 
+  match x, y with 
+  | "Clark Kent", "Superman" 
+  | "Peter Parker", "Spiderman" -> true
+  | _ -> false
+;;
+
 (* Let's use our own pattern matching. Write a function that returns
    whether x is non zero by matching on x *)
 let non_zero x = failwith "For you to implement"
@@ -18,3 +26,12 @@ let non_zero x = failwith "For you to implement"
 let%test "Testing non_zero..." = Bool.( = ) false (non_zero 0)
 let%test "Testing non_zero..." = Bool.( = ) true (non_zero 500)
 let%test "Testing non_zero..." = Bool.( = ) true (non_zero (-400))
+
+(* Now, write a function that returns whether x and y are both
+   positive by matching on both of them. *)
+let both_positive x y = failwith "For you to implement"
+
+let%test "Testing both_positive..." = Bool.( = ) false (both_positive 0 1)
+let%test "Testing both_positive..." = Bool.( = ) false (both_positive 2 (-5))
+let%test "Testing both_positive..." = Bool.( = ) false (both_positive (-20) (-5))
+let%test "Testing both_positive..." = Bool.( = ) true (both_positive 400 14)
--- 02-exercises/06-pattern-matching/problem.mli
+++ 02-exercises/06-pattern-matching/problem.mli
@@ -3,3 +3,5 @@ open! Base
 val non_zero : int -> bool 
 
 val is_superman : string -> bool
+
+val is_same_person : string -> string -> bool
