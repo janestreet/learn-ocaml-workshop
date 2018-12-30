9b6fea0f7f4a1ddf2c548379dfa6da72646a7865
haohangxu <haohang.xu@gmail.com>
2018-12-30 15:19:21 -0500

    adding more exercises

--- 02-exercises/02-basic_types/problem.mli
+++ 02-exercises/02-basic_types/problem.mli
@@ -12,8 +12,12 @@ val int_average   : int   -> int   -> int
 val float_average : float -> float -> float
 
 (* dune automatically checks that every variable defined in an ml file
-   is used. You can see these warnings by running [dune
-   runtest]. These warnings are useful to make sure we didn't forget
+   is used. You can see these warnings by running:
+
+   $ dune clean
+   $ dune runtest
+
+   These warnings are useful to make sure we didn't forget
    to expose something in the mli that we meant to. Uncomment the
    following signatures to resolve dune's unused variable warnings. *)
 (* val four : int
