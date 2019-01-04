043506a89ab9592c72611e206734712df2075577
haohangxu <haohang.xu@gmail.com>
2019-01-04 18:03:52 +0000

    update make_witt.sh script
    Signed-off-by: haohangxu <haohang.xu@gmail.com>

--- make_learn_ocaml_directory.sh
+++ make_learn_ocaml_directory.sh
@@ -2,5 +2,10 @@
 
 mkdir ../learn-ocaml
 cp Makefile ../learn-ocaml/
+
+dune clean
 cp -r 02-exercises/ ../learn-ocaml/01-exercises
 cp -r 03-lumines/ ../learn-ocaml/02-lumines
+
+dune build solutions/lumines/bin/lumines.exe
+cp _build/default/solutions/lumines/bin/lumines.exe ../learn-ocaml/lumines_demo.exe
