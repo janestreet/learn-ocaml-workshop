a3fbd25072b349506c762a4047b6d9188a2d14ef
haohangxu <haohang.xu@gmail.com>
2019-08-25 19:49:34 -0400

    Add some more details about utop in exercise 1
    Signed-off-by: Haohang Xu <haohang.xu@gmail.com>

--- 02-exercises/01-introduction/problem.ml
+++ 02-exercises/01-introduction/problem.ml
@@ -5,17 +5,25 @@
    Write OCaml code using your favorite text editor; if you aren't already
    committed to one, we recommend Visual Studio Code.
 
-   To compile your code and run inline tests, run
+   1. To compile your code and run inline tests, run
 
-   [dune runtest]
+   $ dune runtest
 
    in a terminal session in this problem's directory.
 
    Try building this code.
 
    You should see a compilation error because it's missing the end quote. Add
-   the end quote and re-run. You should see that the code compiled and ran!
+   the end quote and re-run. You should see that the code compiled and ran! 
 
-   You can also execute code in utop directly. Try pasting this line of code
-   into utop and running it there. *)
+   2. OCaml has a toplevel interpreter (i.e. a REPL, or read-eval-print
+   loop) called utop. Try starting up utop in the command line like so:
+
+   $ utop
+
+   You can also execute code in this environment directly. Try
+   pasting the previous line of code into utop and running it there.
+*)
 let () = Stdio.printf "Hello, World!
+
+
