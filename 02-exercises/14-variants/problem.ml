d78c9ab2155b30138ab5f9d8a5e6d1724954a57f
haohangxu <haohang.xu@gmail.com>
2019-08-26 15:57:28 -0400

    Clean up explanations in exercise 14
    Signed-off-by: Haohang Xu <haohang.xu@gmail.com>

--- 02-exercises/14-variants/problem.ml
+++ 02-exercises/14-variants/problem.ml
@@ -1,28 +1,28 @@
 open! Base
 
-(* As in most languages, you can define your own types.
-   The keyword "type" introduces a type definition.
+(* As in most languages, you can define your own types. The keyword [type]
+   introduces a type definition.
 
-   One of the non-basic types in OCaml is called the variant type.
-   Variant types are similar to Enums in other languages. They are
-   types which may take on multiple forms, where each form is marked
-   by an explicit tag. A variant type is defined as follows:
-*)
+   One of the non-basic types in OCaml is called the variant type.  Variant
+   types are similar to Enums in other languages. They are types which may take
+   on multiple forms, where each form is marked by an explicit tag. A variant
+   type is defined as follows: *)
 type color =
   | Red
   | Green
   | Blue
 
-(* Variants are very useful in combination with pattern matching *)
+(* Variants are very useful in combination with pattern matching. (Check out
+   exercise 6 for a refresher on pattern matching.) *)
 let to_string color =
   match color with
   | Red   -> "red"
   | Green -> "green"
   | Blue  -> "blue"
 
-(* OCaml variants are in many ways more powerful than Enums because the different
-   constructors of your variant can include data in them. Here's an example:
-*)
+(* OCaml variants are in many ways more powerful than Enums because the
+   different constructors of your variant can include data in them. Here's an
+   example: *)
 type card_value =
   | Ace
   | King
