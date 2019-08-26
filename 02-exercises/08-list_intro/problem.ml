2acb04ba3e044d890a744166338534efc633865d
haohangxu <haohang.xu@gmail.com>
2019-08-25 22:18:41 -0400

    Clean up explanations in exercise 8
    Signed-off-by: Haohang Xu <haohang.xu@gmail.com>

--- 02-exercises/08-list_intro/problem.ml
+++ 02-exercises/08-list_intro/problem.ml
@@ -1,26 +1,27 @@
 open! Base
 
-(* OCaml natively supports linked lists as part of the language.
-   Lists are commonly referred to as having heads and tails.
-   The head is the first element of the linked list
-   The tail is everything else.
+(* OCaml natively supports linked lists as part of the language.  Lists are
+   commonly referred to as having heads and tails.  The head is the first
+   element of the linked list The tail is everything else.
 
-   To construct a list we use the cons infix operator :: to prepend elements to 
-   the front of a list
+   To construct a list we use the cons infix operator [( :: )] to prepend elements to
+   the front of a list:
 
-   val (::) : 'a -> 'a list -> 'a list
+   [val (::) : 'a -> 'a list -> 'a list]
 
-   [] means "the empty list".
-   hd :: tl means "the element hd added to the front of the list tl".
+   [] means "the empty list".  hd :: tl means "the element hd added to the front
+   of the list tl". 
 
-   When matching on a list, it's either empty or non-empty. To say it another way, it's
-   either equal to [] or equal to (hd :: tl) where hd is the first element of the list
-   and tl is all the rest of the elements of the list (which may itself be empty).
+   The following assertion shows that we can construct lists in two ways.
 *)
-
 let () = assert ([%compare.equal: int list] [ 5; 1; 8; 4 ]  (5 :: 1 :: 8 :: 4  :: []))
 
-(* This function computes the length of a list. *)
+(* When matching on a list, it's either empty or non-empty. To say it another
+   way, it's either equal to [] or equal to (hd :: tl) where hd is the first
+   element of the list and tl is all the rest of the elements of the list (which
+   may itself be empty).
+
+   For example, this function computes the length of a list. *)
 let rec length lst =
   match lst with
   | [] -> 0
@@ -30,18 +31,10 @@ let rec length lst =
 (* Write a function to add up the elements of a list by matching on it. *)
 let rec sum lst = failwith "For you to implement"
 
-(* The signature for the append operator is
-   val (@) : 'a list -> 'a list -> 'a list
+(* The signature for the append infix operator is:
 
-   It's an infix operator.
-*)
+   [val (@) : 'a list -> 'a list -> 'a list] *)
 let list_append first second = first @ second
-
-(* The way you put something on the head to the list
-   uses the same kind of syntax for matching on lists.
-   val (::) : 'a -> 'a list -> 'a list
-*)
-let new_head hd rest = hd :: rest
 
 let%test "Testing sum..." = Int.( = ) 0 (sum [])
 let%test "Testing sum..." = Int.( = ) 55 (sum [ 55 ])
