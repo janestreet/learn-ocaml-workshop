d360f5624b7df0ff50a4f65a1c25433cb731187d
haohangxu <haohang.xu@gmail.com>
2019-08-26 21:05:22 -0400

    Clean up explanations in exercise 19
    Signed-off-by: Haohang Xu <haohang.xu@gmail.com>

--- 02-exercises/19-refs/problem.ml
+++ 02-exercises/19-refs/problem.ml
@@ -1,28 +1,31 @@
 open! Base
 
-(* It is sometimes useful to create a single mutable value. We can do this
-   using a ref. We can create an [int ref] containing 0 as follows:
-*)
+(* It is sometimes useful to create a single mutable value. We can do this using
+   a [ref]. We can create an [int ref] containing 0 as follows. *)
 let x = ref 0
 
-(* Then we can access the value in the ref using the ! operator, and
-   we can update it using the := operator. So, we could increment our
-   ref as follows:
-*)
+(* Then we can access the value in the ref using the [!] operator, and we can
+   update it using the [:=] operator. So, we could increment our ref as
+   follows. *)
 let () =
   x := !x + 1
 
-(* Write a function min_and_max which returns a tuple containing the
-   minimum and maximum values in a non-empty list of positive
-   integers. Your function should raise if the list is empty.
+(* Write a function min_and_max which returns a tuple containing the minimum and
+   maximum values in a non-empty list of positive integers. Your function should
+   raise if the list is empty.
 
-   Your function should iterate over the list and maintain refs of the
-   minimum and maximum values seen so far.  *)
+   You could do this using [List.fold], but for the purpose of this exercise,
+   let's iterate over the list and explicitly maintain refs of the minimum and
+   maximum values seen so far instead. *)
 let min_and_max lst =
   failwith "For you to implement"
 
+(* By the way, can you guess how a [ref] is implemented under the hood? 
+
+   (Hint: exercise 18.) *)
+
 let%test "Testing min_and_max..." =
-  [%compare.equal: int*int] (min_and_max [5;9;2;4;3]) (2,9) 
+  [%compare.equal: int * int] (min_and_max [5;9;2;4;3]) (2,9) 
 ;;
 
 let%test "Testing min_and_max..." =
