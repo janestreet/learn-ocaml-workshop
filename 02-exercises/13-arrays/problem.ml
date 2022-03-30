2c15f36ba791024d8e340d238e3ebc2abbaf32ad
kirisky <youandox@gmail.com>
2022-03-30 23:39:43 +0300

    Solution for 02-13

--- 02-exercises/13-arrays/problem.ml
+++ 02-exercises/13-arrays/problem.ml
@@ -54,7 +54,8 @@ let () =
 
    Let's implement a function [double] using [Array.iteri], which takes an [int
    array] and doubles each element of the array in place. *)
-let double array : unit = failwith "For you to implement"
+let double array : unit = 
+  Array.iteri array ~f:(fun i e -> array.(i) <- e * 2)
 
 let%test "Testing double..." = 
   let array = [| 1; 1; 1 |] in
@@ -72,7 +73,8 @@ let%test "Testing double..." =
 
 (* Write a function that takes an [int array] and a list of indicies and
    doubles each of the elements at the specified indices. *)
-let double_selectively array indices : unit = failwith "For you to implement"
+let double_selectively array indices : unit = 
+  List.iter indices ~f:(fun i -> array.(i) <- array.(i) * 2)
 
 let%test "Testing double_selectively..." = 
   let array = [| 1; 1; 1 |] in
@@ -103,7 +105,8 @@ let () =
 
 (* Write a function that takes an [int array array] and doubles each of the
    elements at the specified indices. *)
-let double_matrix matrix : unit = failwith "For you to implement"
+let double_matrix matrix : unit = 
+  Array.iteri matrix ~f:(fun i e -> Array.iteri e ~f:(fun ii ee -> matrix.(i).(ii) <- ee * 2))
 
 let%test "Testing double_matrix..." = 
   let matrix = [| [| 1; 2; 3 |]; [| 1; 1; 1 |] |] in
