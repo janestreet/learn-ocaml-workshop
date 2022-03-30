1520534dfd3a5e8125ae78cf33ed3a1f19a0f32f
kirisky <youandox@gmail.com>
2022-03-30 21:12:37 +0300

    Solution for 02-11

--- 02-exercises/11-labeled_arguments/problem.ml
+++ 02-exercises/11-labeled_arguments/problem.ml
@@ -48,7 +48,7 @@ let () =
 (* Now, implement [modulo] using our version of divide with labeled
    arguments. Remember that you can look at the mli for the function
    signature. *)
-let modulo ~dividend ~divisor = failwith "For you to implement"
+let modulo ~dividend ~divisor = dividend % divisor
 
 let%test "Testing modulo..." =
   Int.(=) 2 (modulo ~dividend:17 ~divisor:5)
