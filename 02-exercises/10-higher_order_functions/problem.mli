f80f9f114dd621f7a5c1174b1416c2c89c09cac8
kirisky <youandox@gmail.com>
2022-03-30 20:52:46 +0300

    solution for 02-10

--- 02-exercises/10-higher_order_functions/problem.mli
+++ 02-exercises/10-higher_order_functions/problem.mli
@@ -3,3 +3,4 @@ open! Base
 val up_to : int -> (int -> int -> int) -> int -> int
 val simpler_sum : int list -> int
 val simpler_product : int list -> int
+val every : int -> (int -> int -> int) -> int list -> int
